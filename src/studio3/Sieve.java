914a621ee7b5a8e4dc40320e1d0ab31eb4d7456e
luisg <luisg@LuisGComputer.dhcp.wustl.edu>
2024-02-07 15:55:12 -0600

    Code for Studio#3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,30 @@
+package studio3;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Find all prime numebers");  
+		int Length= 40;
+		int[] prime= new int[Length];  
+		for(int n=2 ; n < prime.length; n++) {
+			prime [n]= n +1; 
+					
+			System.out.println(prime[n]); 
+			
+			
+				}
+					
+					
+		
+			
+			
+			
+	} 
+}
+		
+		
+		
+
+
+
